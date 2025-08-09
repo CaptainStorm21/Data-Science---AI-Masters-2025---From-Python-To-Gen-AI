82fea50326bd03ef0243c96deeae416f31862b50
Captain Storm <CaptainStorm21@users.noreply.github.com>
2025-08-08 23:04:46 -0700

    Create 9. Variables & Keywords.py

---
+++ 9. Variables & Keywords.py
@@ -0,0 +1,20 @@
+
+
+
+a="helloworld";
+type(a);
+b=34;
+print(b);
+my_age=34;
+print(my_age);
+bool_var=True;
+print(bool_var);
+price=12.5;
+print(price);
+
+def addMyNums(t, m):
+	return t + m
+
+print(addMyNums(3,5))#Output:8
+
+
