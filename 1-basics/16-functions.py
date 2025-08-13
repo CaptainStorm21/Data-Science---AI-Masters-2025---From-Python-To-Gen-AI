41e5e07dd9bc9af8208e43e76f9cecd9f1f3fc67
Captain Storm <CaptainStorm21@users.noreply.github.com>
2025-08-12 23:22:36 -0700

    Update 16-functions.py

--- 1-basics/16-functions.py
+++ 1-basics/16-functions.py
@@ -1,8 +1,16 @@
 # Functions 
 def function_name (argument1, argument2):
     sum_objects = argument1 + argument2
-    return  sum_objects
+    print(sum_objects)
 
+function_name(3,3)
+
+def fnct_odd_even (num):
+    if num % 2 == 0:
+        print(num, "is even")
+    else: 
+        print(num, "is odd")
+fnct_odd_even(5)
 
 # lambda function
 #   small anon function to make develoers life easier 
