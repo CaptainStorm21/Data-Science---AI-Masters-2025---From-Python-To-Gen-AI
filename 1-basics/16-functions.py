380a643b1bf7545c446a43a2116045d8c8d3c405
Captain Storm <CaptainStorm21@users.noreply.github.com>
2025-08-12 23:17:21 -0700

    Create 16-functions.py

---
+++ 1-basics/16-functions.py
@@ -0,0 +1,11 @@
+# Functions 
+def function_name (argument1, argument2):
+    sum_objects = argument1 + argument2
+    return  sum_objects
+
+
+# lambda function
+#   small anon function to make develoers life easier 
+#   can take any number of argument but have only one 
+#   expression
+
